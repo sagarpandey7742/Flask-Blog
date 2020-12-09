879a2922abb64c0b7268224ff625c90a73d63198
sagarpandey7742 <sagarpandey7742@gmail.com>
2020-12-10 01:42:39 +0530

    Added pagination support and user specific posts page

--- app/routes.py
+++ app/routes.py
@@ -11,8 +11,20 @@ from PIL import Image
 @app.route('/')
 @app.route('/home')
 def home():
-    posts = Post.query.all()
+    page = request.args.get("page", 1, type=int)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
     return render_template("home.html", posts=posts)
+
+
+@app.route('/user/<string:username>')
+def user_posts(username):
+    page = request.args.get("page", 1, type=int)
+    user=User.query.filter_by(username=username).first_or_404()
+    print(user)
+    posts = Post.query.filter_by(author=user)\
+            .order_by(Post.date_posted.desc())\
+            .paginate(page=page, per_page=5)
+    return render_template("user_posts.html", posts=posts, user=user)
 
 
 @app.route('/about')
@@ -116,16 +128,16 @@ def update_post(post_id):
     if post.author != current_user:
         abort(403)
     else:
-        form=PostForm()
+        form = PostForm()
         if form.validate_on_submit():
-            post.title=form.title.data
-            post.content=form.content.data
+            post.title = form.title.data
+            post.content = form.content.data
             db.session.commit()
-            flash(f"Post Updated!",category="success")
+            flash(f"Post Updated!", category="success")
             return redirect(url_for("post", post_id=post.id))
-        elif request.method=="GET":
-            form.title.data=post.title
-            form.content.data=post.content
+        elif request.method == "GET":
+            form.title.data = post.title
+            form.content.data = post.content
         return render_template('create_post.html', title="Update Post", form=form, legend="Update Post")
 
 
@@ -139,4 +151,3 @@ def delete_post(post_id):
     db.session.commit()
     flash(f"Post deleted successfully", category="info")
     return redirect(url_for("home"))
-
