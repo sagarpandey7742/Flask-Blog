74e65547352c5e34394c98923ffc9334a4b5bd73
sagarpandey7742 <sagarpandey7742@gmail.com>
2020-12-09 11:48:31 +0530

    Added password hashing functionality

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,11 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '2ade7dca04722e1dc2aa38dd6c31023898993cf0291432e811cee85e3526e898'
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///site.db"
 db = SQLAlchemy(app)
-
+bcrypt = Bcrypt(app)
 from app import routes
